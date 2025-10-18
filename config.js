f633327d898abb177ff408e01dacbe05034e571c
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-10-18 16:45:19 +0530

    Add ALIVE_IMG and ALIVE_MSG to config

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || ""
+SESSION_ID: process.env.SESSION_ID || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ue4ppc.jpg",
+ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now"
 };
