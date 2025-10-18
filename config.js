aeb82ab9dbe00438ce2acb519c74189f7eb5e7a6
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-10-18 17:19:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "suho~nZ1XVaZC#DyFPXKzSWjfzu8hsHwo5gxihYI6k5KImDb3CaR0AVNo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ue4ppc.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now"
 };
