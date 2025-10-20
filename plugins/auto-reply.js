6d78f5dc1c049429617e16170eec61a78c7409d4
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-10-20 11:38:03 +0530

    Update auto-reply.js

--- plugins/auto-reply.js
+++ plugins/auto-reply.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const config = require('../settings')
+const config = require('../config')
 const { cmd } = require('../command')
 
 //auto reply 
